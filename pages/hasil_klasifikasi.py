44cba254bbf321c841f7391a37575201e933f8ec
Firmansyah Mukti Wijaya <me@ikimukti.com>
2023-11-01 13:35:27 +0700

    jkdkadsfbkhsdf

--- pages/hasil_klasifikasi.py
+++ pages/hasil_klasifikasi.py
@@ -1,5 +1,6 @@
 import tkinter as tk
 import os
+from PIL import Image, ImageTk
 
 def show_hasil_klasifikasi(content_frame):
     label = tk.Label(content_frame, text="Hasil Klasifikasi", font=("Arial", 24))
@@ -8,7 +9,7 @@ def show_hasil_klasifikasi(content_frame):
     # Path ke direktori images/classification/
     classification_dir = "images/classification/"
 
-    # Dapatkan daftar folder di dalam direktori classification_dir
+    # Dapatkan daftar folder di dalam direktori classification_dir  
     folders = [f for f in os.listdir(classification_dir) if os.path.isdir(os.path.join(classification_dir, f))]
 
     # Hitung jumlah folder
@@ -22,10 +23,35 @@ def show_hasil_klasifikasi(content_frame):
     max_folder_width = max(len(folder) for folder in folders)
     button_width = max(max_folder_width + 2, 20)  # 2 spasi ekstra, minimal lebar 20
 
-    # Buat tombol untuk setiap folder
+    # Maksimum jumlah tombol per baris
+    max_buttons_per_row = 4
+
+    # Buat tombol untuk setiap folder dan atur dalam grid
     for i, folder in enumerate(folders):
+        row, col = divmod(i, max_buttons_per_row)
         folder_button = tk.Button(folder_frame, text=folder.capitalize(), width=button_width, command=lambda f=folder: open_classification_folder(f))
-        folder_button.pack(side="left", fill="x")  # Mengatur tombol di sebelah kiri dengan jarak antar tombol
+        folder_button.grid(row=row, column=col, sticky="ew")
+
+    # Buat frame untuk gambar dan atur dalam grid
+    image_frame = tk.Frame(content_frame)
+    image_frame.pack()
+
+    # Ambil gambar dari setiap folder dan tampilkan
+    row_count = 4
+    col_count = 6
+
+    for i, folder in enumerate(folders):
+        image_dir = os.path.join(classification_dir, folder)
+        images = [f for f in os.listdir(image_dir) if f.lower().endswith((".png", ".jpg", ".jpeg", ".gif", ".bmp"))]
+
+        for j, image_file in enumerate(images):
+            image_path = os.path.join(image_dir, image_file)
+            image = Image.open(image_path)
+            image = image.resize((150, 150))  # Atur ukuran gambar
+            tk_image = ImageTk.PhotoImage(image)
+            img_label = tk.Label(image_frame, image=tk_image)
+            img_label.image = tk_image
+            img_label.grid(row=i, column=j, padx=5, pady=5)
 
 def open_classification_folder(folder_name):
     # Fungsi ini akan dijalankan saat tombol folder ditekan.

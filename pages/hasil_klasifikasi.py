8597e3ecd0354693bd12f171b28cd01d19e5930f
Firmansyah Mukti Wijaya <iki.mukti@gmail.com>
2023-11-01 09:20:32 +0700

    asfasgasdhg

--- pages/hasil_klasifikasi.py
+++ pages/hasil_klasifikasi.py
@@ -1,7 +1,33 @@
 import tkinter as tk
+import os
 
 def show_hasil_klasifikasi(content_frame):
-    label = tk.Label(content_frame, text="Halaman Hasil Klasifikasi", font=("Arial", 24))
-    label.pack()
+    label = tk.Label(content_frame, text="Hasil Klasifikasi", font=("Arial", 24))
+    label.pack()  # Menggunakan pack agar berada di tengah
 
-# ...
+    # Path ke direktori images/classification/
+    classification_dir = "images/classification/"
+
+    # Dapatkan daftar folder di dalam direktori classification_dir
+    folders = [f for f in os.listdir(classification_dir) if os.path.isdir(os.path.join(classification_dir, f))]
+
+    # Hitung jumlah folder
+    num_folders = len(folders)
+
+    # Buat frame untuk tombol-tombol folder
+    folder_frame = tk.Frame(content_frame)
+    folder_frame.pack(side="top")  # Mengatur frame di sebelah kiri
+
+    # Menghitung lebar tombol agar berukuran sama
+    max_folder_width = max(len(folder) for folder in folders)
+    button_width = max(max_folder_width + 2, 20)  # 2 spasi ekstra, minimal lebar 20
+
+    # Buat tombol untuk setiap folder
+    for i, folder in enumerate(folders):
+        folder_button = tk.Button(folder_frame, text=folder.capitalize(), width=button_width, command=lambda f=folder: open_classification_folder(f))
+        folder_button.pack(side="left", fill="x")  # Mengatur tombol di sebelah kiri dengan jarak antar tombol
+
+def open_classification_folder(folder_name):
+    # Fungsi ini akan dijalankan saat tombol folder ditekan.
+    # Anda dapat menambahkan logika untuk membuka folder atau melakukan tindakan lain di sini.
+    print(f"Folder {folder_name} terbuka.")
--- pages/proses_klasifikasi.py
+++ pages/proses_klasifikasi.py
@@ -3,6 +3,7 @@ from tkinter import PhotoImage, filedialog
 from PIL import Image, ImageTk
 import cv2
 import numpy as np
+import os  # Untuk manajemen file dan folder
 
 def show_proses_klasifikasi(content_frame):
     # Hapus konten saat ini
@@ -24,15 +25,23 @@ def show_proses_klasifikasi(content_frame):
     # Path gambar terpilih
     selected_image_path = None
 
-    def create_image_label_button(image_path, label_text, row, column, width, height):
+    # Ukuran gambar yang diinginkan
+    image_width = 150
+    image_height = 150
+
+    # Folder untuk menyimpan gambar sementara
+    temp_folder = "images/temp/"
+    os.makedirs(temp_folder, exist_ok=True)  # Buat folder jika belum ada
+
+    def create_image_label_button(image_path, label_text, row, column):
         nonlocal segmentation_button, classification_button
 
         # Gambar
         pil_image = Image.open(image_path)
+        pil_image = pil_image.resize((image_width, image_height))  # Mengatur ukuran gambar
         tk_image = ImageTk.PhotoImage(pil_image)
         image_label = tk.Label(image_frame, image=tk_image)
         image_label.image = tk_image
-        image_label.configure(width=width, height=height)
         image_label.grid(row=row, column=column, padx=10, pady=10, sticky="nsw")
 
         # Label untuk teks di bawah gambar
@@ -46,7 +55,7 @@ def show_proses_klasifikasi(content_frame):
             import_button.grid(row=row + 2, column=column, padx=10, pady=10, sticky="nsew")
         elif label_text == "Gambar Segmentasi":
             button_text = "Segmentation Image"
-            segmentation_button = tk.Button(image_frame, text=button_text, command=lambda: segmentation_image(image_label, label, selected_image_path), state=tk.DISABLED)
+            segmentation_button = tk.Button(image_frame, text=button_text, command=lambda: segmentation_image(image_label, label), state=tk.DISABLED)
             segmentation_button.grid(row=row + 2, column=column, padx=10, pady=10, sticky="nsew")
         elif label_text == "Gambar Klasifikasi":
             button_text = "Classification Image"
@@ -57,56 +66,62 @@ def show_proses_klasifikasi(content_frame):
         nonlocal selected_image_path
         file_path = filedialog.askopenfilename(title="Pilih Gambar", filetypes=[("Image files", "*.png *.jpg *.jpeg *.gif *.bmp")])
         if file_path:
-            selected_image_path = file_path  # Simpan path gambar terpilih
+            # Simpan gambar terpilih sementara di folder "images/temp/"
+            filename = os.path.basename(file_path)
+            temp_path = os.path.join(temp_folder, filename)
+            os.makedirs(os.path.dirname(temp_path), exist_ok=True)
+            os.replace(file_path, temp_path)
+            selected_image_path = temp_path
 
             # Buka gambar menggunakan Pillow (PIL)
-            pil_image = Image.open(file_path)
-
-            # Dapatkan ukuran gambar
-            width, height = pil_image.size
-
-            # Ganti gambar "Gambar Input" dengan gambar yang dipilih
-            pil_image = pil_image.resize((width, height))
+            pil_image = Image.open(temp_path)
+            pil_image = pil_image.resize((image_width, image_height))  # Mengatur ukuran gambar
             tk_image = ImageTk.PhotoImage(pil_image)
             image_label.configure(image=tk_image)
             image_label.image = tk_image
-            label.configure(text=f"Gambar Input (Terpilih)\nWidth: {width}, Height: {height}")
+            label.configure(text=f"Gambar Input (Terpilih)\nWidth: {image_width}, Height: {image_height}")
 
             # Aktifkan tombol Segmentation dan Classification
             if segmentation_button and classification_button:
                 segmentation_button.configure(state=tk.NORMAL)
                 classification_button.configure(state=tk.NORMAL)
 
-    def segmentation_image(image_label, label, image_path):
-        # Dapatkan gambar dari label "Gambar Input (Terpilih)"
-        image = image_label.cget("image")
-
-        if image:
-            # Konversi gambar Tkinter (PIL) ke gambar OpenCV
-            tk_image = ImageTk.getimage(image)
-            open_cv_image = np.array(tk_image)
-            open_cv_image = cv2.cvtColor(open_cv_image, cv2.COLOR_RGB2BGR)
+    def segmentation_image(image_label, label):
+        if selected_image_path:
+            # Buka gambar terpilih
+            pil_image = Image.open(selected_image_path)
 
             # Lakukan segmentasi gambar menggunakan Adaptive Thresholding
+            open_cv_image = np.array(pil_image)
+            open_cv_image = cv2.cvtColor(open_cv_image, cv2.COLOR_RGB2BGR)
             gray = cv2.cvtColor(open_cv_image, cv2.COLOR_BGR2GRAY)
-            segmented_image = cv2.adaptiveThreshold(gray, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 11, 2)
+
+            # Konfigurasi Adaptive Thresholding
+            block_size = 11
+            constant = 2
+
+            segmented_image = cv2.adaptiveThreshold(gray, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, block_size, constant)
 
             # Tampilkan gambar hasil segmentasi pada "Gambar Segmentasi"
             pil_segmented_image = Image.fromarray(segmented_image)
+            pil_segmented_image = pil_segmented_image.resize((image_width, image_height))
             tk_segmented_image = ImageTk.PhotoImage(pil_segmented_image)
             image_label.configure(image=tk_segmented_image)
             image_label.image = tk_segmented_image
 
+            # Deskripsi tambahan
+            description = f"Gambar Segmentasi\n(Metode: Adaptive Thresholding)"
+            label.configure(text=description, font=("Arial", 9))
+
+
     # Gambar pertama
-    create_image_label_button("images/gambar.png", "Gambar Input", 0, 0, 150, 150)
+    create_image_label_button("images/gambar.png", "Gambar Input", 0, 0)
 
     # Gambar kedua
-    create_image_label_button("images/gambar.png", "Gambar Segmentasi", 0, 1, 150, 150)
+    create_image_label_button("images/gambar.png", "Gambar Segmentasi", 0, 1)
 
     # Gambar ketiga
-    create_image_label_button("images/gambar.png", "Gambar Klasifikasi", 0, 2, 150, 150)
+    create_image_label_button("images/gambar.png", "Gambar Klasifikasi", 0, 2)
 
     def button_click(label):
         print(f"Tombol ditekan untuk: {label.cget('text')}")
-
-# Memanggil fungsi show_proses_klasifikasi jika Anda ingin mengeksekusi tampilan ini.

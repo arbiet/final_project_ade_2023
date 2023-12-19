85994f11f1eb3a0378f833c2b20660579067b97f
Firmansyah Mukti Wijaya <me@ikimukti.com>
2023-12-20 01:29:04 +0700

    asfsafas

--- pages/latihmodel_klasifikasi.py
+++ pages/latihmodel_klasifikasi.py
@@ -4,162 +4,171 @@ import os
 import cv2
 import numpy as np
 from sklearn import svm
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import accuracy_score
-from sklearn.preprocessing import StandardScaler  # Import StandardScaler for feature scaling
+from sklearn.model_selection import train_test_split, GridSearchCV
+from sklearn.metrics import accuracy_score, classification_report
+from sklearn.preprocessing import StandardScaler
 import joblib
 from PIL import Image
 
-# Add a global variable to store file names and their classification results
-file_results = []
+def extract_features(image_path):
+    # Membaca citra menggunakan OpenCV
+    image = cv2.imread(image_path)
 
-def load_images(folder_path, label, image_size=(500, 500), apply_noise_reduction=True):
-    images = []
-    labels = []
+    # Periksa apakah citra dapat dibaca
+    if image is None:
+        print(f"Warning: Failed to read image at {image_path}")
+        # Berikan nilai khusus atau tanggapi sesuai kebutuhan
+        return np.zeros(769)  # Memastikan dimensi fitur tetap
 
-    try:
-        for image_name in os.listdir(folder_path):
-            image_path = os.path.join(folder_path, image_name)
+    # Langkah 1: Ekstraksi Ukuran Citra
+    size_feature = np.prod(image.shape)
 
-            # Attempt to read the image
-            original_image = Image.open(image_path)
-            pil_image = Image.open(image_path)
+    # Langkah 2: Segmentasi menggunakan deteksi tepi Canny
+    gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    edges = cv2.Canny(gray_image, 50, 150)
 
-            # Consider processing the image with blurring or other processing techniques
-            processed_image = cv2.GaussianBlur(np.array(pil_image), (5, 5), 0)
+    # Langkah 3: Ekstraksi Histogram dari hasil segmentasi
+    hist_edges = cv2.calcHist([edges], [0], None, [256], [0, 256])
 
-            # Perform segmentation using Canny Edge Detection
-            open_cv_image = np.array(processed_image)
-            open_cv_image = cv2.cvtColor(open_cv_image, cv2.COLOR_RGB2BGR)
-            gray = cv2.cvtColor(open_cv_image, cv2.COLOR_BGR2GRAY)
+    # Langkah 4: Ekstraksi Histogram Warna dari citra asli
+    # Konversi citra ke ruang warna HSV
+    hsv_image = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
 
-            # Consider morphological processing (dilation and erosion)
-            kernel = np.ones((5, 5), np.uint8)
-            gray = cv2.dilate(gray, kernel, iterations=1)
-            gray = cv2.erode(gray, kernel, iterations=1)
+    # Hitung histogram warna untuk setiap saluran (Hue, Saturation, Value)
+    hist_hue = cv2.calcHist([hsv_image], [0], None, [256], [0, 256])
+    hist_saturation = cv2.calcHist([hsv_image], [1], None, [256], [0, 256])
+    hist_value = cv2.calcHist([hsv_image], [2], None, [256], [0, 256])
 
-            # Detect edges using the Canny operator
-            lower_threshold = 30
-            upper_threshold = 100
-            edges = cv2.Canny(gray, lower_threshold, upper_threshold)
+    # Gabungkan histogram menjadi satu vektor fitur
+    color_feature = np.concatenate([hist_hue.flatten(), hist_saturation.flatten(), hist_value.flatten(), hist_edges.flatten()])
 
-            # Example: Apply Hough Transform to detect straight lines
-            lines = cv2.HoughLines(edges, 1, np.pi / 180, threshold=50)
-
-            # If you want to combine the detected lines, you can add a merging process here
-            kernel_line = np.ones((5, 5), np.uint8)
-            dilated_lines = cv2.dilate(edges, kernel_line, iterations=1)
-            closed_lines = cv2.morphologyEx(dilated_lines, cv2.MORPH_CLOSE, kernel_line, iterations=2)
-
-            # Resize the segmented image after line joining
-            resized_segmented_image = cv2.resize(closed_lines, image_size)
-
-            # Apply noise reduction (Gaussian blur)
-            if apply_noise_reduction:
-                resized_segmented_image = cv2.GaussianBlur(resized_segmented_image, (3, 3), 0)
-
-            # Invert the segmented image
-            inverted_segmented_image = cv2.bitwise_not(resized_segmented_image)
-
-            # Append the inverted segmented image to the list
-            images.append(inverted_segmented_image)
-            labels.append(label)
-
-        return images, labels
-
-    except Exception as e:
-        print(f"Error loading images from '{folder_path}': {e}")
+    # Perbaikan: Menggunakan np.array daripada np.prod untuk size_feature
+    return np.concatenate([np.array([size_feature]), color_feature])
 
 def train_svm_model(progress_var, label_result):
-    global file_results  # Use a global variable to store file names and their results
-    # Function to train the SVM model
-    # Adjust the paths based on your project structure
-    folder_matang = "images/classification/matang"
-    folder_busuk = "images/classification/busuk"
-    folder_kehijauan = "images/classification/kehijauan"
-    folder_kering = "images/classification/kering"
-    folder_test = "images/classification/test"
+    global file_results  # Gunakan variabel global
 
-    # Load training images from different folders
-    matang_images, matang_labels = load_images(folder_matang, 0, apply_noise_reduction=True)
-    busuk_images, busuk_labels = load_images(folder_busuk, 1, apply_noise_reduction=True)
-    kehijauan_images, kehijauan_labels = load_images(folder_kehijauan, 2, apply_noise_reduction=True)
-    kering_images, kering_labels = load_images(folder_kering, 3, apply_noise_reduction=True)
+    # Langkah 2: Pengumpulan Data Latih
+    folder_paths = ["images/classification/busuk", "images/classification/kehijauan", "images/classification/kering", "images/classification/matang"]
+    data = []
+    labels = []
 
+    # Mendapatkan path absolut dari direktori saat ini
+    # current_directory = os.path.dirname(os.path.abspath(__file__))
 
-    # Combine all data for training
-    images = np.vstack((matang_images, busuk_images, kehijauan_images, kering_images))
-    labels = np.hstack((matang_labels, busuk_labels, kehijauan_labels, kering_labels))
+    for i, folder_name in enumerate(folder_paths):
+        # folder_path = os.path.join(current_directory, folder_name)
+        
+        # Periksa apakah folder ada
+        if not os.path.exists(folder_name):
+            print(f"Folder {folder_name} tidak ditemukan.")
+            continue
+        
+        file_list = os.listdir(folder_name)
+        for file_name in file_list:
+            image_path = os.path.join(folder_name, file_name)
+            # Langkah 3: Ekstraksi Fitur
+            features = extract_features(image_path)
+            data.append(features)
+            labels.append(i)
 
-    # Define class names
-    class_names = {
-        0: 'Matang',
-        1: 'Busuk',
-        2: 'Kehijauan',
-        3: 'Kering'
-    }
+    # Menampilkan contoh data latih
+    print("Contoh data latih:")
+    print("Fitur Citra\t\t\t\t\t\tLabel")
+    for i in range(3):
+        print(f"{data[i]}\t\t{labels[i]}")
 
-    # Split the data into training and testing sets
-    X_train, X_test, y_train, y_test = train_test_split(images, labels, test_size=0.25, random_state=42)
+    # Langkah 4: Pembagian Data
+    X_train, X_test, y_train, y_test = train_test_split(data, labels, test_size=0.2, random_state=42)
 
-    # Preprocess the data: Flatten images and scale features
-    X_train_flat = X_train.reshape(X_train.shape[0], -1)
-    X_test_flat = X_test.reshape(X_test.shape[0], -1)
-
+    # Langkah 5: Normalisasi Data
     scaler = StandardScaler()
-    X_train_scaled = scaler.fit_transform(X_train_flat)
-    X_test_scaled = scaler.transform(X_test_flat)
+    X_train = scaler.fit_transform(X_train)
+    X_test = scaler.transform(X_test)
 
-    # Create and train the SVM model
-    model = svm.SVC(kernel='poly', C=1.0, probability=True, gamma="auto")
-    model.fit(X_train_scaled, y_train)
+    # Menampilkan contoh data latih setelah normalisasi
+    print("Contoh data latih setelah normalisasi:")
+    print("Fitur Citra (Setelah Normalisasi)\t\t\tLabel")
+    for i in range(3):
+        print(f"{X_train[i]}\t\t{y_train[i]}")
 
-    # Save the model along with class names
-    joblib.dump((model, scaler, class_names), 'pages/svm_model.joblib')
+    # Langkah 6: Pelatihan Model SVM
+    clf = svm.SVC(kernel='linear', C=1.0)
+    clf.fit(X_train, y_train)
 
-    # Make predictions on the test set
-    y_pred_test = model.predict(X_test_scaled)
+    # Langkah 7: Validasi Model
+    y_pred = clf.predict(X_test)
+    accuracy = accuracy_score(y_test, y_pred)
+    report = classification_report(y_test, y_pred)
 
-    # Calculate accuracy on the test set
+    print(f"Accuracy: {accuracy}")
+    print("Classification Report:")
+    print(report)
+
+    # Langkah 8: Optimasi Model
+    # Contoh optimasi dengan Grid Search untuk mencari parameter terbaik
+    param_grid = {'C': [0.1, 1, 10], 'kernel': ['linear', 'poly', 'rbf']}
+    grid_search = GridSearchCV(svm.SVC(), param_grid, cv=3)
+    grid_search.fit(X_train, y_train)
+
+    # Model terbaik setelah optimasi
+    best_model = grid_search.best_estimator_
+
+    # Menampilkan parameter terbaik
+    print("Parameter terbaik setelah optimasi:", grid_search.best_params_)
+
+    # Langkah 9: Prediksi
+    # Misalnya, kita menggunakan beberapa citra uji yang tidak terlibat dalam pelatihan
+    new_image_paths = ["images/classification/test/kering_2.jpg", "images/classification/test/matang_6.jpg", "images/classification/test/busuk_19.jpg", "images/classification/test/kehijauan_1.jpg", "images/classification/test/matang_1.jpg", "images/classification/test/matang_3.jpg", "images/classification/test/matang_4.jpg", "images/classification/test/matang_5.jpg"]
+    new_data = []
+    dummy_true_labels = [3, 4, 1, 2, 4, 4, 4, 4]  # Sesuaikan dengan label sebenarnya citra uji
+
+    for image_path, true_label in zip(new_image_paths, dummy_true_labels):
+        # Periksa keberadaan file sebelum mencoba membaca citra
+        # image_path = os.path.join(current_directory, image_path)
+
+        # Langkah 3: Ekstraksi Fitur pada citra uji
+        new_features = extract_features(image_path)
+        new_data.append(new_features)
+
+    # Langkah 5: Normalisasi Data pada citra uji
+    new_data = scaler.transform(new_data)
+
+    # Langkah 9: Prediksi kelas citra uji menggunakan model terbaik
+    predictions = best_model.predict(new_data)
+
+    # Menampilkan hasil prediksi
+    label_result.config(text=label_result.cget("text") + "\nHasil Prediksi untuk Citra Uji:")
+    for i, (prediction, true_label) in enumerate(zip(predictions, dummy_true_labels)):
+        label_result.config(text=label_result.cget("text") + f"\nCitra {i+1}: Prediksi Kelas {prediction + 1}, Label Sebenarnya {true_label}")
+
+    # Langkah 10: Evaluasi Hasil
+    # Evaluasi hasil prediksi menggunakan metrik evaluasi yang sesuai
+    y_pred_test = best_model.predict(X_test)
     accuracy_test = accuracy_score(y_test, y_pred_test)
+    report_test = classification_report(y_test, y_pred_test)
 
-    # Load test images
-    test_images, test_labels = load_images(folder_test, None, apply_noise_reduction=True)  # None as label, as it's for testing
+    label_result.config(text=label_result.cget("text") + f"\n\nEvaluasi pada Data Uji:")
+    label_result.config(text=label_result.cget("text") + f"\nAccuracy: {accuracy_test}")
+    label_result.config(text=label_result.cget("text") + "\nClassification Report:")
+    label_result.config(text=label_result.cget("text") + f"\n{report_test}")
 
-    # Preprocess test data
-    test_images_flat = np.array(test_images).reshape(len(test_images), -1)
-    test_images_scaled = scaler.transform(test_images_flat)
-
-    # Make predictions on the test images
-    y_pred_test_images = model.predict(test_images_scaled)
-
-    # Display file names and results in the GUI for the test set
-    file_results = list(zip(os.listdir(folder_test), y_pred_test_images))
-    file_result_message = "\nFile Names and Classification Results for Test Set:\n"
-    # for file_name, result in file_results:
-        # file_result_message += f"{file_name}: {class_names[result]}\n"
-
-    # Update the label_result text
-    label_result.config(text=f"Training complete. Accuracy on test set: {accuracy_test:.2%}\n" + file_result_message)
+    # Menyimpan model ke dalam file
+    model_filename = "model_svm.joblib"
+    joblib.dump(best_model, model_filename)
+    label_result.config(text=label_result.cget("text") + f"\n\nModel SVM telah disimpan sebagai {model_filename}")
 
 def show_latihmodel_klasifikasi(content_frame):
     def train_model_and_display_result():
-        global file_results  # Use the global variable
+        global file_results  # Gunakan variabel global
 
         # Function to train the SVM model and display the result
         progress_var.set(0)  # Reset progress bar
         label_result.config(text="Training is in progress...\n")
         content_frame.update_idletasks()  # Update the content frame
 
+        # Panggil fungsi train_svm_model yang baru
         train_svm_model(progress_var, label_result)
-
-        # Display file names and results in the GUI
-        file_result_message = "\nFile Names and Classification Results:\n"
-        # for file_name, result in file_results:
-            # file_result_message += f"{file_name}: ({result})\n"
-
-        # Update the label_result text
-        label_result.config(text=label_result.cget("text") + file_result_message)
 
     label = tk.Label(content_frame, text="Halaman Latih Model Klasifikasi", font=("Arial", 24))
     label.pack(pady=20)
@@ -175,3 +184,5 @@ def show_latihmodel_klasifikasi(content_frame):
     label_result = tk.Label(content_frame, text="", font=("Arial", 12), wraplength=400, justify="left")
     label_result.pack(pady=10)
 
+# Gunakan variabel file_results yang diubah menjadi global di awal kodingan
+file_results = []
